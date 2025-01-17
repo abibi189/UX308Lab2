72d9914b32a7707ad1eeeb1c48abbeaf236bcbfd
abibi189 <145033589+abibi189@users.noreply.github.com>
2025-01-17 23:03:21 +0000

    add distance.js

---
+++ distance.js
@@ -0,0 +1,27 @@
+// This program *converts* **kilometres** to **miles**
+
+let kilometres = 5;
+
+let miles = kilometres * 0.621371;
+
+console.log(`${kilometres} km is ${miles} miles`)
+
+// This program *converts* **numeric grade** to **letter grade**
+
+let numericgrade = 75;
+
+let lettergrade = 'C'
+
+console.log(numericgrade + " is " + lettergrade);
+
+// This program *calculate* **volume** of a **cylinder**
+
+let diameter = 5;
+
+let height = 2;
+
+let radius = diameter / 2;
+
+let volume = Math.PI * radius * radius * height;
+
+console.log("Volume is " + volume);
